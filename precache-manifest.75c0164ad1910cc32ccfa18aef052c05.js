f78f7bca96dc170c2b756f276bcf2d688d3d3a67
Matt McMurray <MattMcMurray@users.noreply.github.com>
2020-08-17 17:32:29 -0500

    Updates

---
+++ precache-manifest.75c0164ad1910cc32ccfa18aef052c05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8252775900e9a4b683f7aa5ececc9b",
+    "url": "/guitar-map/index.html"
+  },
+  {
+    "revision": "71906445faf85f951996",
+    "url": "/guitar-map/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "26f5b9181da2e4a7c3bc",
+    "url": "/guitar-map/static/css/main.b74f4691.chunk.css"
+  },
+  {
+    "revision": "71906445faf85f951996",
+    "url": "/guitar-map/static/js/2.2d7a3f8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/guitar-map/static/js/2.2d7a3f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f5b9181da2e4a7c3bc",
+    "url": "/guitar-map/static/js/main.5ecfd4f9.chunk.js"
+  },
+  {
+    "revision": "473c16d1bb6570ed9166",
+    "url": "/guitar-map/static/js/runtime-main.8c96e5cc.js"
+  }
+]);
