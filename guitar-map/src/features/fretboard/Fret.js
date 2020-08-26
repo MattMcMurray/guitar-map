bafa0ce1449256704dbf71600d28baaa24c7dd3d
Matt McMurray <MattMcMurray@users.noreply.github.com>
2020-08-26 12:15:26 -0500

    Add validation on input Fret

--- guitar-map/src/features/fretboard/Fret.js
+++ guitar-map/src/features/fretboard/Fret.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Input } from "antd";
+import { Input, Form } from "antd";
 import PropTypes from "prop-types";
+
+import { ALL_VALID_NOTE_INPUTS } from "../../constants";
+
+const DEFAULT_BG_COLOUR = "#f6ffed";
+const DEFAULT_ERR_COLOUR = "#ff4d4f";
+const DEFAULT_BORDER_COLOUR = "#d9f7be";
 
 export function Fret(props) {
   const { notes } = useSelector((state) => state.controls);
+
+  const [baseNoteValid, setBaseNoteValid] = useState(true);
+  const [bgColour, setBgColour] = useState(
+    props.overrideColour ? props.overrideColour : DEFAULT_BG_COLOUR
+  );
+
   const isSelected = notes[props.children]
     ? notes[props.children]["state"]
     : false;
-  const defaultColour = props.overrideColour ? props.overrideColour : "#f6ffed";
-  const borderColour = "#d9f7be";
+
+  const validateBaseNote = (value) => {
+    if (
+      !ALL_VALID_NOTE_INPUTS.includes(
+        value.substring(0, 1).toUpperCase() + value.substring(1)
+      )
+    ) {
+      setBaseNoteValid(false);
+      setBgColour(DEFAULT_ERR_COLOUR);
+    } else {
+      setBaseNoteValid(true);
+      setBgColour(
+        props.overrideColour ? props.overrideColour : DEFAULT_BG_COLOUR
+      );
+    }
+  };
+
+  const onBaseNoteChange = (value) => {
+    if (baseNoteValid) {
+      props.onNoteChange(value);
+    }
+  };
 
   // TODO: break out editable fret into its own component
   return (
@@ -17,10 +49,10 @@ export function Fret(props) {
       style={{
         backgroundColor: isSelected
           ? notes[props.children]["colour"]
-          : defaultColour,
+          : bgColour,
         height: "50px",
         width: props.width,
-        border: `1px ${borderColour} solid`,
+        border: `1px ${DEFAULT_BORDER_COLOUR} solid`,
         color: isSelected || props.overrideColour != null ? "white" : "black",
         fontWeight: "bold",
         display: "flex",
@@ -29,15 +61,26 @@ export function Fret(props) {
       }}
     >
       {props.editable ? (
-        // TODO: add validation
-        <Input
-          placeholder="?"
-          bordered={false}
-          value={props.children}
-          style={{ color: "white", textAlign: "center", fontWeight: "bold" }}
-          onBlur={e => props.onNoteChange(e.target.value)}
-          onPressEnter={e => props.onNoteChange(e.target.value)}
-        />
+        <Form>
+          <Form.Item
+            style={{ marginBottom: 0 }}
+            validateStatus={baseNoteValid ? "success" : "error"}
+          >
+            <Input
+              placeholder="?"
+              bordered={false}
+              value={props.children}
+              style={{
+                color: "white",
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+              onChange={(e) => validateBaseNote(e.target.value)}
+              onBlur={(e) => onBaseNoteChange(e.target.value)}
+              onPressEnter={(e) => onBaseNoteChange(e.target.value)}
+            />
+          </Form.Item>
+        </Form>
       ) : (
         props.children
       )}
